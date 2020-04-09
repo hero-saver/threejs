7a47f388186e3fda84a0dd2ad0aa0fa65e7fac2b
Xenon472 <andrea.lucchesi95@hotmail.it>
2020-04-09 19:14:20 +0200

    test2

--- src/finalizeMesh.js
+++ src/finalizeMesh.js
@@ -38,7 +38,8 @@ finalizeMesh.prototype = {
                         newGeometry.attributes.position.setXYZ(i, vertex.x, vertex.y, vertex.z);
                     } else {
                         var finalVector = new Vector4();
-                        /*if (geometry.morphTargetInfluences !== undefined) {
+						
+                        if (geometry.morphTargetInfluences !== undefined) {
 
                             var morphVector = new Vector4(vertex.x, vertex.y, vertex.z);
                             var tempMorph = new Vector4();
@@ -54,8 +55,9 @@ finalizeMesh.prototype = {
 
                                 tempMorph.addScaledVector(morph.sub(morphVector), geometry.morphTargetInfluences[mt]);
                             }
-                            morphVector.add(tempMorph);
-                        }*/
+							//comment to avoid morph problems
+                            //morphVector.add(tempMorph);
+                        }
 
                         for (var si = 0; si < geometry.skinIndexNames.length; si++) {
 
@@ -87,11 +89,11 @@ finalizeMesh.prototype = {
                             skinMatrices[3] = mesh.skeleton.bones[skinIndex[3]].matrixWorld;
 
                             for (var k = 0; k < 4; k++) {
-                                /*if (geometry.morphTargetInfluences !== undefined) {
+                                if (geometry.morphTargetInfluences !== undefined) {
                                     var tempVector = new Vector4(morphVector.x, morphVector.y, morphVector.z);
-                                } else {*/
-                                var tempVector = new Vector4(vertex.x, vertex.y, vertex.z);
-                                //}
+                                } else {
+									var tempVector = new Vector4(vertex.x, vertex.y, vertex.z);
+                                }
 
                                 tempVector.multiplyScalar(skinWeight[k]);
                                 //the inverse takes the vector into local bone space
